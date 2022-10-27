457a86cdafe673a9886d95fa0baf02969d808ed7
UhaiMong <uhaimarma2016@gmail.com>
2022-10-27 13:26:59 +0600

    hosting vercel

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const cors = require('cors');
 app.use(cors());
 
 const port = process.env.PORT || 5000;
-const tutorialData = require("./data/tutorials.json");
 const courseName = require("./data/course-name.json");
+const tutorialData = require("./data/tutorials.json");
 
 
 app.get('/', (req, res) => {
@@ -19,12 +19,17 @@ app.get('/course-name', (req, res) => {
 // dynamic fetch with id.
 app.get('/tutorial/:id', (req, res) => {
     const id = req.params.id;
-    const getSingleTutorial = tutorialData.find(tutorial => tutorial.id == id);
+    const getSingleTutorial = tutorialData.find(tutorial => tutorial.id === id);
+
     if (!getSingleTutorial) {
         res.send("No data found");
     }
     res.send(getSingleTutorial);
 });
+// All tutorial data send to client side
+app.get('/all-tutorial', (req, res) => {
+    res.send(tutorialData);
+})
 
 app.listen(port, () => {
     console.log("The server is running on the ", port);
